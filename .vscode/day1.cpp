258d427048a6479e3c6c3170fa394db581249b3c
Arjin Joshi <thearjinjoshi1@gmail.com>
2023-02-12 02:51:37 +0545

    Day 1 [Updated]

--- .vscode/day1.cpp
+++ .vscode/day1.cpp
@@ -8,46 +8,33 @@ int main()
 
     cout << !(a == 2) << endl;
 
-    // if (a <= 3 && b >= 3)
-    //
-    //     cout << "LImit Reached" << endl;
-    // }
+    if (a <= 3 && b >= 3)
+    {
+        cout << "LImit Reached" << endl;
+    }
 
-    // cout << (5.0) / 3 << endl;
-    // cout << (5) / 3.0;
+    cout << (5.0) / 3 << endl;
+    cout << (5) / 3.0;
 
-    // char ch = 1024;
-    // cout << ch << endl;
+    char ch = 99;
+    cout << ch << endl;
 
-    // double d = 5.7;
-    // int x = (int)d + 2;
-    // cout << "value of x:" << x << endl;
+    double d = 5.7;
+    int x = (int)d + 2;
+    cout << "value of x:" << x << endl;
 
-    // char ch = 97;
-    // cout << ch << endl;
+    char ab = 97;
+    cout << ab << endl;
 
-    // int num = 'b';
-    // cout << num << endl;
+    int num = 'b';
+    cout << num << endl;
 
-    // long a= 5;
-    // cout<< sizeof(a)<<endl;
+    long e = 5;
+    cout << sizeof(e) << endl;
 
-    // cout << 2;
-    // cout << endl;
-    // cout << "Arjin\n"
-    //      << endl;
-    // cout << '\n';
-
-    // cout << "Enter a number\n";
-    // int a;
-    // // taking input
-    // cin >> a;
-    // cout << "You entered" << a << endl;
-
-    // int a = 5;
-    // cout << "value of a is :" << a << endl;
-    // char ch = 'a';
-    // cout << "value of ch is:" << ch << endl;
-    // bool flag = true;
-    // cout << "value of flag is:" << flag << endl;
+    cout << 2;
+    cout << endl;
+    cout << "Arjin\n"
+         << endl;
+    cout << '\n';
 }
